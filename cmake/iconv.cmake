1f6c2c5e64b50470735d8b9f2d0fa90b8bcaf532
blackknife <a1991815a@gmail.com>
2018-12-22 02:15:03 +0800

    添加gzip tar解压工具

--- cmake/iconv.cmake
+++ cmake/iconv.cmake
@@ -1,20 +1,38 @@
 add_library(libiconv STATIC 
-    ${LIBICONV_ROOT}/src/iconv.c
-    ${LIBICONV_ROOT}/libcharset/src/localcharset.c
+    ${LIBICONV_ROOT}/lib/iconv.c
+    ${LIBICONV_ROOT}/libcharset/lib/localcharset.c
 )
 
-target_include_directories(libiconv ${LIBICONV_ROOT}/include)
-target_include_directories(libiconv ${LIBICONV_ROOT}/libcharset/include)
-target_include_directories(libiconv ${LIBICONV_ROOT}/lib)
+target_include_directories(libiconv PUBLIC 
+    ${LIBICONV_ROOT}/include
+    ${LIBICONV_ROOT}/lib
+    ${LIBICONV_ROOT}/libcharset/include
+    ${TEMP_PATH}/iconv
+    )
 
-file(STRINGS ${LIBICONV_ROOT}/include/iconv.h.in ICONV_FILE)
-string(REGEX REPLACE "@DLL_VARIABLE@" "" ICONV_FILE ${ICONV_FILE})
-string(REGEX REPLACE "@EILSEQ@" "42" ICONV_FILE ${ICONV_FILE})
-string(REGEX REPLACE "@" "" ICONV_FILE ${ICONV_FILE})
-file(WRITE ${LIBICONV_ROOT}/include/iconv.h ${ICONV_FILE})
+set(EILSEQ 42)
+set(USE_MBSTATE_T "USE_MBSTATE_T")
+set(BROKEN_WCHAR_H "0")
+set(HAVE_WCHAR_T "HAVE_WCHAR_T")
+set(ICONV_CONST "const")
+set(HAVE_ICONV "1")
+configure_file(${LIBICONV_ROOT}/include/iconv.h.in ${TEMP_PATH}/iconv/iconv.h @ONLY)
+
+file(READ ${LIBICONV_ROOT}/lib/config.h.in ICONV_FILE)
+
+string(REPLACE "#undef HAVE_ICONV" "#define HAVE_ICONV 1" ICONV_FILE ${ICONV_FILE})
+string(REPLACE "#define ICONV_CONST" "#define ICONV_CONST const" ICONV_FILE ${ICONV_FILE})
+string(REPLACE "#undef HAVE_STRING_H" "#define HAVE_STRING_H 1" ICONV_FILE ${ICONV_FILE})
+string(REPLACE "#undef HAVE_STDLIB_H" "#define HAVE_STDLIB_H 1" ICONV_FILE ${ICONV_FILE})
+string(REPLACE "#undef HAVE_STDDEF_H" "#define HAVE_STDDEF_H 1" ICONV_FILE ${ICONV_FILE})
+file(WRITE ${TEMP_PATH}/iconv/config.h ${ICONV_FILE})
 set(ICONV_FILE)
+set(EILSEQ)
+set(USE_MBSTATE_T)
+set(BROKEN_WCHAR_H)
+set(HAVE_WCHAR_T)
+set(ICONV_CONST)
+set(HAVE_ICONV)
 
-file(COPY ${LIBICONV_ROOT}/libcharset/include/libcharset.h.in DESTINATION ${LIBICONV_ROOT}/libcharset/include/libcharset.h)
-file(COPY ${LIBICONV_ROOT}/libcharset/include/localcharset.h.in DESTINATION ${LIBICONV_ROOT}/libcharset/include/localcharset.h)
-
-file(COPY ${LIBICONV_ROOT}/lib/config.h.in DESTINATION ${LIBICONV_ROOT}/lib/config.h)
+configure_file(${LIBICONV_ROOT}/libcharset/include/libcharset.h.in ${TEMP_PATH}/iconv/libcharset.h @ONLY)
+configure_file(${LIBICONV_ROOT}/libcharset/include/localcharset.h.in ${TEMP_PATH}/iconv/localcharset.h @ONLY)
