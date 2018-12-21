259d90b50814092baedf4eaf54405bcb226b6f4e
lkq <lkq@zhiqim.com>
2018-12-21 15:41:08 +0800

    添加一键清理脚本 实现一键编译脚本

--- build.bat
+++ build.bat
@@ -1,4 +1,24 @@
 @echo off
-vcvarsall %1
-devenv /BUILD Release Depdencies.sln
+where cmake
+if %errorlevel% NEQ 0 set path=%path%;C:\Program Files\CMake\bin
+
+where cl
+if %errorlevel% NEQ 0 call vcvarsall x86_amd64
+
+if not exist build mkdir build
+if not exist build64 mkdir build64
+
+pushd build
+cmake -G "Visual Studio 15 2017" --build ..
+popd
+pushd build64
+cmake -G "Visual Studio 15 2017 Win64" --build ..
+popd
+
+devenv /BUILD Release "build/*.sln"
+devenv /BUILD Release "build64/*.sln"
+
+devenv /BUILD Release /Project "INSTALL" "build/*.sln"
+devenv /BUILD Release /Project "INSTALL" "build64/*.sln"
+
 @echo on
---
+++ clean.bat
@@ -0,0 +1,8 @@
+@echo off
+echo remove build files......
+if exist build rmdir /S /Q build
+if exist build64 rmdir /S /Q build64
+if exist out rmdir /S /Q out
+if exist out64 rmdir /S /Q out64
+echo success.
+@echo on
