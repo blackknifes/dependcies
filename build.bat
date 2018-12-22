c3db7ade1f5cd254e1d204f0f4947e25a9da0b2f
克勤 李 <a1991815a@gmail.com>
2018-12-23 00:13:32 +0800

    使用生成器字符串判断生成的目标平台来决定输出路径

--- build.bat
+++ build.bat
@@ -4,6 +4,11 @@ if %errorlevel% NEQ 0 set path=%path%;C:\Program Files\CMake\bin
 
 where cl
 if %errorlevel% NEQ 0 call vcvarsall x86_amd64
+where cl
+if %errorlevel% NEQ 0 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall x86_amd64"
+
+where devenv
+if %errorlevel% NEQ 0 set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE
 
 if not exist build mkdir build
 if not exist build64 mkdir build64
--- clean.bat
+++ clean.bat
@@ -4,5 +4,6 @@ if exist build rmdir /S /Q build
 if exist build64 rmdir /S /Q build64
 if exist out rmdir /S /Q out
 if exist out64 rmdir /S /Q out64
+if exist temp rmdir /S /Q temp
 echo success.
 @echo on
